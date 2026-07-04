91a7ca71280eda2f881821c3d7950e2cc35c0e00
michal2812 <258569567+michal2812@users.noreply.github.com>
2026-07-04 10:31:52 +0000

    chore: bump cloudgrid to 0.11.0

--- Formula/cloudgrid.rb
+++ Formula/cloudgrid.rb
@@ -1,8 +1,8 @@
 class Cloudgrid < Formula
   desc "Build, ship, and run apps and agents on CloudGrid from your terminal"
   homepage "https://github.com/cloudgrid-io/cli"
-  url "https://registry.npmjs.org/@cloudgrid-io/cli/-/cli-0.9.16.tgz"
-  sha256 "ade600ceb3642f3433a3f58753dc468d46974b7cb17bdb467c9f79a2b11f57d1"
+  url "https://registry.npmjs.org/@cloudgrid-io/cli/-/cli-0.11.0.tgz"
+  sha256 "f047a4a3d48957ee0d3770ec359c22a105ae85fcb2eed33e0449edd00e214ca1"
   license "Apache-2.0"
 
   depends_on "node"
